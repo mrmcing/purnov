f8630ea018c17784194e954d4874a70804b109f0
Anatolii Krivonos <greymix092@gmail.com>
2017-06-21 11:49:47 +0300

    верстка - страница переподписки

---
+++ index.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+
+    <meta charset="utf-8">
+
+    <title>Марафон</title>
+    <meta name="description" content="">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- Bootstrap -->
+    <link href="js/libs/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="fonts/Font-Awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+<section class="preview">
+
+    <div class="img-graph-wrapper chart" style="width: 0px;">
+        <img src="img/icon/bars.png" alt="" class="reletive">
+    </div>
+
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <a class="brand" href="http://purnov.com/" target="_blank">alexander<span class="text-gold">purnov</span></a>
+                </div>
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <p class="tagline">...сегодня я торгую лучше, чем вчера</p>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <h1>хочу остаться в школе трейдинга</h1>
+                <p class="sub-title">александра пурнова!</p>
+            </div>
+            <div class="col-xs-12 text-center">
+                <div class="arrow"></div>
+            </div>
+            <div class="col-xs-12">
+                <div class="box-form">
+                    <form class="form-horizontal">
+                        <div class="form-group">
+                            <div class="col-md-4">
+                                <label class="sr-only" for="name">Ваше имя</label>
+                                <input type="text" class="form-control" pattern="^[A-Za-zа-яА-ЯёЁ`-]{2,30}" name="name" id="name" placeholder="Ваше Имя..." required="">
+                            </div>
+                            <div class="col-md-4">
+                                <label class="sr-only" for="email">Ваш e-mail...</label>
+                                <input type="email" class="form-control" pattern="^[_A-Za-z0-9-\\+]+(.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(.[A-Za-z0-9]+)*(.[A-Za-z]{2,})$" id="email" name="email" placeholder="Ваш e-mail..." required="">
+                            </div>
+                            <div class="col-md-4">
+                                <label class="sr-only" for="tel">Ваш телефон...</label>
+                                <input type="tel" class="form-control" pattern="^\+[0-9]{10,16}|" title="Украина:+380ххххххххх, Россия:+79ххххххххх" id="tel" name="phone" placeholder="Ваш телефон..." required="">
+                            </div>
+
+                            <div class="col-md-4 col-md-offset-4">
+                                <button type="submit" class="btn">отправить</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<footer>
+
+    <div class="panel-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-5 col-xs-12">
+                    <a class="brand" href="http://purnov.com/" target="_blank">alexander<span class="text-gold">purnov</span></a>
+                </div>
+                <div class="col-md-9 col-sm-7 col-xs-12">
+                    <div class="social-box">
+                        <a class="icon-wrapper hide" href="#" target="_blank"><i class="fa fa-vk custom-icon"></i></a>
+                        <a class="icon-wrapper" href="https://www.youtube.com/user/ishitrade" target="_blank"><i class="fa fa-youtube custom-icon"></i></a>
+                        <a class="icon-wrapper hide" href="#" target="_blank"><i class="fa fa-odnoklassniki custom-icon"></i></a>
+                        <a class="icon-wrapper" href="https://plus.google.com/u/0/+%D0%A8%D0%BA%D0%BE%D0%BB%D0%B0%D0%A2%D1%80%D0%B5%D0%B9%D0%B4%D0%B8%D0%BD%D0%B3%D0%B0%D0%90%D0%BB%D0%B5%D0%BA%D1%81%D0%B0%D0%BD%D0%B4%D1%80%D0%B0%D0%9F%D1%83%D1%80%D0%BD%D0%BE%D0%B2%D0%B0/posts" target="_blank"><i class="fa fa-google-plus custom-icon"></i></a>
+                        <a class="icon-wrapper hide" href="#" target="_blank"><i class="fa fa-instagram custom-icon"></i></a>
+                        <a class="icon-wrapper" href="https://www.facebook.com/alexpurnovschool" target="_blank"><i class="fa fa-facebook custom-icon"></i></a>
+                        <a class="icon-wrapper" href="https://twitter.com/alexpurnov" target="_blank"><i class="fa fa-twitter custom-icon"></i></a>
+                        <a class="icon-wrapper hide" href="#" target="_blank"><i class="fa fa-linkedin custom-icon"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-sm-2">
+                <ul>
+                    <li><a href="http://purnov.com/" target="_blank">Главная</a></li>
+                    <li><a href="http://purnov.com/about-alexander.php" target="_blank">Об Александре</a></li>
+                    <li><a href="http://purnov.com/teaching.php" target="_blank">Об обучении</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 col-sm-3">
+                <ul>
+                    <li><a href="http://blog.purnov.com/" target="_blank">Блог</a></li>
+                    <li><a href="http://purnov.com/course-scheduling.php" target="_blank">Расписание</a></li>
+                    <li><a href="https://www.youtube.com/user/ishitrade/playlists" target="_blank">Бесплатные материалы</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 col-sm-3">
+                <ul>
+                    <li><a href="http://purnov.com/sitemap.htm" target="_blank">Карта сайта</a></li>
+                    <li><a href="#" data-toggle="modal" data-target="#disclaimer">Дисклеймер</a></li>
+                    <li><a href="#" data-toggle="modal" data-target="#confidentiality">Политика конфиденциальности</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 col-sm-4">
+                <ul>
+                    <li>
+                        <i class="fa fa-phone" aria-hidden="true"></i>
+                        <a href="tel:+74996772081">+7 (499) 6772081</a>
+                    </li>
+                    <li>
+                        <i class="fa fa-skype" aria-hidden="true"></i> скайп Школы -
+                        <a href="skype:purnov school">purnov school</a>,
+                    </li>
+                    <li>
+                        <i class="fa fa-envelope" aria-hidden="true"></i> e-mal -
+                        <a href="mailto:alexpurnovschol@gmail.com">alexpurnovschol@gmail.com</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <p class="legal-information">ИННИП: 343602446190 ОГРНИП: 317344300045990</p>
+                <p class="copyright"><i class="fa fa-copyright" aria-hidden="true"></i> Школа трейдинга Александра Пурнова, 2017</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/libs/bootstrap/js/bootstrap.min.js"></script>
+<script src="js/common.js"></script>
+
+
+<!-- Начало. Подключение скриптов в футер. -->
+<?// include $_SERVER['DOCUMENT_ROOT']."/class/footer_script.php";
+//$foother_object = new foother_object();
+//echo $foother_object->trimodal,
+//$foother_object->google_analytics,
+//$foother_object->jivosite,
+//$foother_object->zadarma;
+//?>
+<!-- Конец. Подключение скриптов в футер. -->
+
+</body>
+</html>
---
+++ js/common.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    $('.img-graph-wrapper').animate({width:'100%'}, 7000);
+});
